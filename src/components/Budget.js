0265e2b424d0c23759a1a281b45abcb5c17015df
amukta malyada <66318372+amuktamalyadap@users.noreply.github.com>
2023-02-24 23:17:03 +0530

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,12 +1,59 @@
 
+
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
-const Budget = () => {
+const Budget = ({currency}) => {
     const { budget } = useContext(AppContext);
+    if(currency == "rupee"){
     return (
+      
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget ₹: {budget}</span>
         </div>
+     
+    
     );
+    }
+    else if (currency == "dollar"){
+        return (
+          
+            <div className='alert alert-secondary'>
+                <span>Budget $: {budget}</span>
+            </div>
+         
+        
+        );
+        }
+        else if (currency == "pound"){
+            return (
+              
+                <div className='alert alert-secondary'>
+                    <span>Budget £: {budget}</span>
+                </div>
+             
+            
+            );
+            }
+            else if (currency == "euro"){
+                return (
+                  
+                    <div className='alert alert-secondary'>
+                        <span>Budget €: {budget}</span>
+                    </div>
+                 
+                
+                );
+                }
+            else {
+                return (
+                  
+                    <div className='alert alert-secondary'>
+                        <span>Budget £: {budget}</span>
+                    </div>
+                 
+                
+                );
+                }
 };
 export default Budget;
+
