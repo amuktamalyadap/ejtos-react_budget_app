f929e3e22bcbae752775f662a251ac9c7972b499
amukta malyada <66318372+amuktamalyadap@users.noreply.github.com>
2023-02-24 23:15:56 +0530

    Update AllocationForm.js

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -3,7 +3,7 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
-const AllocationForm = (props) => {
+const AllocationForm = ({currency}) => {
     const { dispatch,remaining  } = useContext(AppContext);
 
     const [name, setName] = useState('');
@@ -35,6 +35,7 @@ const AllocationForm = (props) => {
             }
     };
 
+if(currency == "rupee"){
     return (
         <div>
             <div className='row'>
@@ -60,7 +61,10 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                
+                    <span>  ₹</span>
+                
+                  
                     <input
                         required='required'
                         type='number'
@@ -78,6 +82,198 @@ const AllocationForm = (props) => {
 
         </div>
     );
+}
+else if(currency == "dollar"){
+    return (
+        <div>
+            <div className='row'>
+
+            <div className="input-group mb-3" style={{ marginLeft: '2rem' }}>
+                    <div className="input-group-prepend">
+                <label className="input-group-text" htmlFor="inputGroupSelect01">Department</label>
+                  </div>
+                  <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
+                        <option defaultValue>Choose...</option>
+                        <option value="Marketing" name="marketing"> Marketing</option>
+                <option value="Sales" name="sales">Sales</option>
+                <option value="Finance" name="finance">Finance</option>
+                <option value="HR" name="hr">HR</option>
+                <option value="IT" name="it">IT</option>
+                <option value="Admin" name="admin">Admin</option>
+                  </select>
+
+                    <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
+                <label className="input-group-text" htmlFor="inputGroupSelect02">Allocation</label>
+                  </div>
+                  <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
+                        <option defaultValue value="Add" name="Add">Add</option>
+                <option value="Reduce" name="Reduce">Reduce</option>
+                  </select>
+                
+                    <span>  $</span>
+                
+                  
+                    <input
+                        required='required'
+                        type='number'
+                        id='cost'
+                        value={cost}
+                        style={{ marginLeft: '2rem' , size: 10}}
+                        onChange={(event) => setCost(event.target.value)}>
+                        </input>
+
+                    <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
+                        Save
+                    </button>
+                </div>
+                </div>
+
+        </div>
+    );
+}
+else if(currency == "euro"){
+    return (
+    <div>
+        <div className='row'>
+
+        <div className="input-group mb-3" style={{ marginLeft: '2rem' }}>
+                <div className="input-group-prepend">
+            <label className="input-group-text" htmlFor="inputGroupSelect01">Department</label>
+              </div>
+              <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
+                    <option defaultValue>Choose...</option>
+                    <option value="Marketing" name="marketing"> Marketing</option>
+            <option value="Sales" name="sales">Sales</option>
+            <option value="Finance" name="finance">Finance</option>
+            <option value="HR" name="hr">HR</option>
+            <option value="IT" name="it">IT</option>
+            <option value="Admin" name="admin">Admin</option>
+              </select>
+
+                <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
+            <label className="input-group-text" htmlFor="inputGroupSelect02">Allocation</label>
+              </div>
+              <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
+                    <option defaultValue value="Add" name="Add">Add</option>
+            <option value="Reduce" name="Reduce">Reduce</option>
+              </select>
+            
+                <span>  €</span>
+            
+              
+                <input
+                    required='required'
+                    type='number'
+                    id='cost'
+                    value={cost}
+                    style={{ marginLeft: '2rem' , size: 10}}
+                    onChange={(event) => setCost(event.target.value)}>
+                    </input>
+
+                <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
+                    Save
+                </button>
+            </div>
+            </div>
+
+    </div>
+);}
+else if (currency == "pound"){
+    return (
+    <div>
+        <div className='row'>
+
+        <div className="input-group mb-3" style={{ marginLeft: '2rem' }}>
+                <div className="input-group-prepend">
+            <label className="input-group-text" htmlFor="inputGroupSelect01">Department</label>
+              </div>
+              <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
+                    <option defaultValue>Choose...</option>
+                    <option value="Marketing" name="marketing"> Marketing</option>
+            <option value="Sales" name="sales">Sales</option>
+            <option value="Finance" name="finance">Finance</option>
+            <option value="HR" name="hr">HR</option>
+            <option value="IT" name="it">IT</option>
+            <option value="Admin" name="admin">Admin</option>
+              </select>
+
+                <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
+            <label className="input-group-text" htmlFor="inputGroupSelect02">Allocation</label>
+              </div>
+              <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
+                    <option defaultValue value="Add" name="Add">Add</option>
+            <option value="Reduce" name="Reduce">Reduce</option>
+              </select>
+            
+                <span>  £</span>
+            
+              
+                <input
+                    required='required'
+                    type='number'
+                    id='cost'
+                    value={cost}
+                    style={{ marginLeft: '2rem' , size: 10}}
+                    onChange={(event) => setCost(event.target.value)}>
+                    </input>
+
+                <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
+                    Save
+                </button>
+            </div>
+            </div>
+
+    </div>
+);}
+else {
+    return (
+    <div>
+        <div className='row'>
+
+        <div className="input-group mb-3" style={{ marginLeft: '2rem' }}>
+                <div className="input-group-prepend">
+            <label className="input-group-text" htmlFor="inputGroupSelect01">Department</label>
+              </div>
+              <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
+                    <option defaultValue>Choose...</option>
+                    <option value="Marketing" name="marketing"> Marketing</option>
+            <option value="Sales" name="sales">Sales</option>
+            <option value="Finance" name="finance">Finance</option>
+            <option value="HR" name="hr">HR</option>
+            <option value="IT" name="it">IT</option>
+            <option value="Admin" name="admin">Admin</option>
+              </select>
+
+                <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
+            <label className="input-group-text" htmlFor="inputGroupSelect02">Allocation</label>
+              </div>
+              <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
+                    <option defaultValue value="Add" name="Add">Add</option>
+            <option value="Reduce" name="Reduce">Reduce</option>
+              </select>
+            
+                <span>  £</span>
+            
+              
+                <input
+                    required='required'
+                    type='number'
+                    id='cost'
+                    value={cost}
+                    style={{ marginLeft: '2rem' , size: 10}}
+                    onChange={(event) => setCost(event.target.value)}>
+                    </input>
+
+                <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
+                    Save
+                </button>
+            </div>
+            </div>
+
+    </div>
+);}
+
+    
 };
 
 export default AllocationForm;
