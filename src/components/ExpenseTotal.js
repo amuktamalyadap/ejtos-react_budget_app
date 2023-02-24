91e322c5806f96425948de45f970bc80727421f6
amukta malyada <66318372+amuktamalyadap@users.noreply.github.com>
2023-02-24 23:18:57 +0530

    Update ExpenseTotal.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,16 +1,51 @@
 
 
+
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
-const ExpenseTotal = () => {
+const ExpenseTotal = ({currency}) => {
     const { expenses } = useContext(AppContext);
     const totalExpenses = expenses.reduce((total, item) => {
         return (total += item.cost);
     }, 0);
+
+
+    if(currency == "pound"){
     return (
         <div className='alert alert-primary'>
             <span>Spent so far: £{totalExpenses}</span>
         </div>
     );
+    }
+
+   else if(currency == "dollar"){
+        return (
+            <div className='alert alert-primary'>
+                <span>Spent so far: ${totalExpenses}</span>
+            </div>
+        );
+        }
+        else if(currency == "rupee"){
+            return (
+                <div className='alert alert-primary'>
+                    <span>Spent so far: ₹{totalExpenses}</span>
+                </div>
+            );
+            }
+            else if(currency == "euro"){
+                return (
+                    <div className='alert alert-primary'>
+                        <span>Spent so far: €{totalExpenses}</span>
+                    </div>
+                );
+                }
+            else {
+                return (
+                    <div className='alert alert-primary'>
+                        <span>Spent so far: £{totalExpenses}</span>
+                    </div>
+                );
+                }
+            
 };
 export default ExpenseTotal;
