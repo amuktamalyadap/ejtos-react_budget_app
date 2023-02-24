677791b8846214d1d28dd05375d5cd3bfd1b6bb0
amukta malyada <66318372+amuktamalyadap@users.noreply.github.com>
2023-02-24 23:19:25 +0530

    Update Remaining.js

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,16 +1,57 @@
 
+
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext'; 
-const Remaining = () => {
+const Remaining = ({currency}) => {
     const { expenses, budget } = useContext(AppContext);
     const totalExpenses = expenses.reduce((total, item) => {
         return (total = total + item.cost);
     }, 0);
     const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
+   
+   if(currency == "pound"){
+   
     return (
         <div className={`alert ${alertType}`}>
             <span>Remaining: £{budget - totalExpenses}</span>
         </div>
     );
+   }
+
+  else if(currency == "dollar"){
+   
+    return (
+        <div className={`alert ${alertType}`}>
+            <span>Remaining: ${budget - totalExpenses}</span>
+        </div>
+    );
+   }
+
+   else if(currency == "rupee"){
+   
+    return (
+        <div className={`alert ${alertType}`}>
+            <span>Remaining:  ₹{budget - totalExpenses}</span>
+        </div>
+    );
+   }
+   else if(currency == "euro"){
+   
+    return (
+        <div className={`alert ${alertType}`}>
+            <span>Remaining:  €{budget - totalExpenses}</span>
+        </div>
+    );
+   }
+   else {
+   
+    return (
+        <div className={`alert ${alertType}`}>
+            <span>Remaining: £ {budget - totalExpenses}</span>
+        </div>
+    );
+   }
 };
 export default Remaining;
+
+
