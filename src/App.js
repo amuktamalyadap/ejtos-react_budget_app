90bc50d6ded8df5f0543e9fdc0d16e25ccc86282
amukta malyada <66318372+amuktamalyadap@users.noreply.github.com>
2023-02-24 23:20:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { AppProvider } from './context/AppContext';
@@ -8,32 +9,48 @@ import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
 import RemainingBudget from './components/Remaining';
 
+
+
 const App = () => {
+
+    const[currency,setCurrency]=useState('');
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                 <div className='row mt-3'>
                     <div className='col-sm'>
-                        <Budget />
+                        <Budget currency = {currency}/>
                     </div>
                     <div className='col-sm'>
-                        <RemainingBudget />
+                        <RemainingBudget currency = {currency}/>
                     </div>
                     <div className='col-sm'>
-                        <ExpenseTotal />
+                        <ExpenseTotal currency = {currency}/>
                     </div>
                 </div>
+                
+                <select className="custom-select bg-success" id="currencyselect" onChange={(event) => setCurrency(event.target.value)}>
+                <option defaultValue>Currency(£Pound)</option>        
+                <option value="pound" name="pound">£Pound</option>
+                        <option value="rupee" name="rupee">₹Rupee</option>
+                
+                <option value="dollar" name="dollar">$Dollar</option>
+                <option value="euro" name="euro">€Euro</option>
+              
+                  </select>
+                  
+                  
                 <h3 className='mt-3'>Allocation</h3>
                 <div className='row '>
                     <div className='col-sm'>
-                        <ExpenseList />
+                        <ExpenseList currency = {currency}/>
                     </div>
                 </div>
                 <h3 className='mt-3'>Change allocation</h3>
                 <div className='row mt-3'>
                     <div className='col-sm'>
-                        <AllocationForm/>
+                        <AllocationForm currency = {currency}/>
                     </div>
                 </div>
             </div>
