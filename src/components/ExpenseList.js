c86f00c4d71e28761c9fc00f35bf429b28c7ee65
amukta malyada <66318372+amuktamalyadap@users.noreply.github.com>
2023-02-24 23:18:30 +0530

    Update ExpenseList.js

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,28 +1,154 @@
 
+
 import React, { useContext } from 'react';
 import ExpenseItem from './ExpenseItem';
 import { AppContext } from '../context/AppContext';
 
-const ExpenseList = () => {
+const ExpenseList = ({currency}) => {
     const { expenses } = useContext(AppContext);
     
+
+    if(currency=="pound"){
+    
     return (
+      
         <table className='table'>
               <thead className="thead-light">
             <tr>
               <th scope="col">Department</th>
               <th scope="col">Allocated Budget</th>
               <th scope="col">Increase by 10</th>
+              <th scope="col">Decrease by 10</th>
               <th scope="col">Delete</th>
             </tr>
           </thead>
+          
             <tbody>
+               
             {expenses.map((expense) => (
-                <ExpenseItem id={expense.id} key={expense.id} name={expense.name} cost={expense.cost} />
+                <ExpenseItem id={expense.id} key={expense.id} name={expense.name} cost={"£" +expense.cost} />
             ))}
+
             </tbody>
+
         </table>
+         
     );
+            }
+
+            else if(currency=="dollar"){
+    
+                return (
+                  
+                    <table className='table'>
+                          <thead className="thead-light">
+                        <tr>
+                          <th scope="col">Department</th>
+                          <th scope="col">Allocated Budget</th>
+                          <th scope="col">Increase by 10</th>
+                          <th scope="col">Decrease by 10</th>
+                          <th scope="col">Delete</th>
+                        </tr>
+                      </thead>
+                      
+                        <tbody>
+                           
+                        {expenses.map((expense) => (
+                            <ExpenseItem id={expense.id} key={expense.id} name={expense.name} cost={"$" +expense.cost} />
+                        ))}
+            
+                        </tbody>
+            
+                    </table>
+                     
+                );
+                        }
+
+                        else if(currency=="rupee"){
+    
+                            return (
+                              
+                                <table className='table'>
+                                      <thead className="thead-light">
+                                    <tr>
+                                      <th scope="col">Department</th>
+                                      <th scope="col">Allocated Budget</th>
+                                      <th scope="col">Increase by 10</th>
+                                      <th scope="col">Decrease by 10</th>
+                                      <th scope="col">Delete</th>
+                                    </tr>
+                                  </thead>
+                                  
+                                    <tbody>
+                                       
+                                    {expenses.map((expense) => (
+                                        <ExpenseItem id={expense.id} key={expense.id} name={expense.name} cost={"₹" +expense.cost} />
+                                    ))}
+                        
+                                    </tbody>
+                        
+                                </table>
+                                 
+                            );
+                                    }
+
+                                    else if(currency=="euro"){
+    
+                                        return (
+                                          
+                                            <table className='table'>
+                                                  <thead className="thead-light">
+                                                <tr>
+                                                  <th scope="col">Department</th>
+                                                  <th scope="col">Allocated Budget</th>
+                                                  <th scope="col">Increase by 10</th>
+                                                  <th scope="col">Decrease by 10</th>
+                                                  <th scope="col">Delete</th>
+                                                </tr>
+                                              </thead>
+                                              
+                                                <tbody>
+                                                   
+                                                {expenses.map((expense) => (
+                                                    <ExpenseItem id={expense.id} key={expense.id} name={expense.name} cost={"€" +expense.cost} />
+                                                ))}
+                                    
+                                                </tbody>
+                                    
+                                            </table>
+                                             
+                                        );
+                                                }
+
+                                    else{
+    
+                                        return (
+                                          
+                                            <table className='table'>
+                                                  <thead className="thead-light">
+                                                <tr>
+                                                  <th scope="col">Department</th>
+                                                  <th scope="col">Allocated Budget</th>
+                                                  <th scope="col">Increase by 10</th>
+                                                  <th scope="col">Decrease by 10</th>
+                                                  <th scope="col">Delete</th>
+                                                </tr>
+                                              </thead>
+                                              
+                                                <tbody>
+                                                   
+                                                {expenses.map((expense) => (
+                                                    <ExpenseItem id={expense.id} key={expense.id} name={expense.name} cost={"£" +expense.cost} />
+                                                ))}
+                                    
+                                                </tbody>
+                                    
+                                            </table>
+                                             
+                                        );
+                                                }
+            
 };
 
 export default ExpenseList;
+
